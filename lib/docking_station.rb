80788e68de3ef901af162544744adaa84fcca54e
Paul Cooper <paul_cooper_pc@hotmail.com>
2021-08-25 14:27:46 +0000

    Challenge 10 unfinished

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,16 @@
+class Bike
+
+  def working?
+  
+  end
+  
+end
+
 class DockingStation
     
     def release_bike
-
+      @bike = bike
+      "Bike Released"
     end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,14 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+  end
+
+describe DockingStation do
+  it 'releases a bike' do
+      expect(DockingStation(release_bike)).to eq'Bike Released'
+  end 
+  
+  it 'expects bike to be working' do
+      expect(Bike(working?)).to eq'Bike is working'
+  end
 end
